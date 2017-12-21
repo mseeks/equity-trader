59396b72031fbd0e9e710b7851820d67d7d8c752
Matthew Sullivan <matthew@msull92.com>
2017-12-20 19:31:04 -0600

    Working rewrite

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/antonholmquist/jason"
 	"github.com/segmentio/kafka-go"
 	"github.com/shopspring/decimal"
-	resty "gopkg.in/resty.v1"
+	"gopkg.in/resty.v1"
 )
 
 type signalMessage struct {
@@ -20,155 +20,265 @@ type signalMessage struct {
 	At    string `json:"at"`
 }
 
-func buyInto(symbol string) error {
-	resty.SetRedirectPolicy(resty.FlexibleRedirectPolicy(10))
-
+func accountMetaInformation() (string, decimal.Decimal, error) {
 	resp, err := resty.R().
 		SetQueryParams(map[string]string{}).
 		SetHeader("Accept", "application/json").
 		SetHeader("Authorization", fmt.Sprint("Token ", os.Getenv("ROBINHOOD_TOKEN"))).
 		Get("https://api.robinhood.com/accounts/")
 	if err != nil {
-		return err
+		return "", decimal.Decimal{}, err
 	}
 
 	if resp.StatusCode() != 200 {
-		return fmt.Errorf("Incorrect status code: %v", resp.Status())
+		return "", decimal.Decimal{}, fmt.Errorf("Incorrect status code: %v", resp.Status())
 	}
 
 	value, err := jason.NewObjectFromBytes(resp.Body())
 	if err != nil {
-		return err
+		return "", decimal.Decimal{}, err
 	}
 
 	results, err := value.GetObjectArray("results")
 	if err != nil {
-		return err
+		return "", decimal.Decimal{}, err
 	}
 
 	accountNumber, err := results[0].GetString("account_number")
 	if err != nil {
-		return err
+		return "", decimal.Decimal{}, err
 	}
 
 	buyingPower, err := results[0].GetString("buying_power")
 	if err != nil {
-		return err
+		return "", decimal.Decimal{}, err
 	}
 
 	buyingPowerAsDecimal, err := decimal.NewFromString(buyingPower)
 	if err != nil {
-		return err
+		return "", decimal.Decimal{}, err
 	}
 
-	cashForBuy := buyingPowerAsDecimal.Mul(decimal.NewFromFloat(0.3)).Round(2)
+	return accountNumber, buyingPowerAsDecimal, nil
+}
 
-	fmt.Println(cashForBuy)
-
-	resp, err = resty.R().
-		SetQueryParams(map[string]string{}).
-		SetHeader("Accept", "application/json").
-		Get(fmt.Sprint("https://api.robinhood.com/quotes/", symbol, "/"))
-
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode() != 200 {
-		return fmt.Errorf("Incorrect status code: %v", resp.Status())
-	}
-
-	value, err = jason.NewObjectFromBytes(resp.Body())
-	if err != nil {
-		return err
-	}
-
-	lastTradePrice, err := value.GetString("last_trade_price")
-	if err != nil {
-		return err
-	}
-
-	lastTradePriceAsDecimal, err := decimal.NewFromString(lastTradePrice)
-	if err != nil {
-		return err
-	}
-
-	if lastTradePriceAsDecimal.Cmp(cashForBuy) == 1 {
-		return fmt.Errorf("Skipping order, not enough buying power")
-	}
-
-	count := cashForBuy.Div(lastTradePriceAsDecimal).Floor()
-	fmt.Println("BUY", count, "x", symbol, "@", lastTradePriceAsDecimal.Round(2))
-
-	resp, err = resty.R().
+func instrumentIDFromSymbol(symbol string) (string, error) {
+	resp, err := resty.R().
 		SetQueryParams(map[string]string{
 			"symbol": symbol,
 		}).
 		SetHeader("Accept", "application/json").
 		Get("https://api.robinhood.com/instruments/")
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	if resp.StatusCode() != 200 {
-		return fmt.Errorf("Incorrect status code: %v", resp.Status())
+		return "", fmt.Errorf("Incorrect status code: %v", resp.Status())
 	}
 
-	value, err = jason.NewObjectFromBytes(resp.Body())
+	value, err := jason.NewObjectFromBytes(resp.Body())
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	results, err = value.GetObjectArray("results")
+	results, err := value.GetObjectArray("results")
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	instrumentID, err := results[0].GetString("id")
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	resp, err = resty.R().
+	return instrumentID, nil
+}
+
+func positionMetaInformation(accountNumber string, instrumentID string) (decimal.Decimal, string, error) {
+	resp, err := resty.R().
 		SetHeader("Accept", "application/json").
 		SetHeader("Authorization", fmt.Sprint("Token ", os.Getenv("ROBINHOOD_TOKEN"))).
 		Get(fmt.Sprint("https://api.robinhood.com/positions/", accountNumber, "/", instrumentID, "/"))
 	if err != nil {
-		return err
+		return decimal.Decimal{}, "", err
 	}
 
 	if resp.StatusCode() != 200 {
-		return fmt.Errorf("Incorrect status code: %v", resp.Status())
+		return decimal.Decimal{}, "", fmt.Errorf("Incorrect status code: %v", resp.Status())
 	}
 
-	value, err = jason.NewObjectFromBytes(resp.Body())
+	value, err := jason.NewObjectFromBytes(resp.Body())
 	if err != nil {
-		return err
+		return decimal.Decimal{}, "", err
+	}
+
+	instrumentUrl, err := value.GetString("instrument")
+	if err != nil {
+		return decimal.Decimal{}, "", err
 	}
 
 	ownedQuantity, err := value.GetString("quantity")
 	if err != nil {
-		return err
+		return decimal.Decimal{}, "", err
 	}
 
 	ownedQuantityAsDecimal, err := decimal.NewFromString(ownedQuantity)
 	if err != nil {
+		return decimal.Decimal{}, "", err
+	}
+
+	return ownedQuantityAsDecimal, instrumentUrl, nil
+}
+
+func lastTradePriceForSymbol(symbol string) (decimal.Decimal, error) {
+	resp, err := resty.R().
+		SetQueryParams(map[string]string{}).
+		SetHeader("Accept", "application/json").
+		Get(fmt.Sprint("https://api.robinhood.com/quotes/", symbol, "/"))
+
+	if err != nil {
+		return decimal.Decimal{}, err
+	}
+
+	if resp.StatusCode() != 200 {
+		return decimal.Decimal{}, fmt.Errorf("Incorrect status code: %v", resp.Status())
+	}
+
+	value, err := jason.NewObjectFromBytes(resp.Body())
+	if err != nil {
+		return decimal.Decimal{}, err
+	}
+
+	lastTradePrice, err := value.GetString("last_trade_price")
+	if err != nil {
+		return decimal.Decimal{}, err
+	}
+
+	lastTradePriceAsDecimal, err := decimal.NewFromString(lastTradePrice)
+	if err != nil {
+		return decimal.Decimal{}, err
+	}
+
+	return lastTradePriceAsDecimal, nil
+}
+
+func buyInto(symbol string) error {
+	accountNumber, buyingPower, err := accountMetaInformation()
+	if err != nil {
 		return err
 	}
 
-	if ownedQuantityAsDecimal.Cmp(decimal.NewFromFloat(0.0)) == 1 {
+	cashForBuy := buyingPower.Mul(decimal.NewFromFloat(0.3)).Round(2)
+
+	lastTradePrice, err := lastTradePriceForSymbol(symbol)
+	if err != nil {
+		return err
+	}
+
+	if lastTradePrice.Cmp(cashForBuy) == 1 {
+		return fmt.Errorf("Skipping order, not enough buying power")
+	}
+
+	instrumentID, err := instrumentIDFromSymbol(symbol)
+	if err != nil {
+		return err
+	}
+
+	ownedQuantity, instrumentUrl, err := positionMetaInformation(accountNumber, instrumentID)
+	if err != nil {
+		return err
+	}
+
+	if ownedQuantity.Cmp(decimal.NewFromFloat(0.0)) == 1 {
 		return fmt.Errorf("Skipping order, already own equity")
+	}
+
+	count := cashForBuy.Div(lastTradePrice).Floor()
+	fmt.Println("BUY", count, "x", symbol, "@", lastTradePrice.Round(2))
+
+	resp, err := resty.R().
+		SetBody(map[string]interface{}{
+			"account":       fmt.Sprint("https://api.robinhood.com/accounts/", accountNumber, "/"),
+			"instrument":    instrumentUrl,
+			"symbol":        symbol,
+			"type":          "market",
+			"trigger":       "immediate",
+			"quantity":      count,
+			"price":         lastTradePrice.Round(2),
+			"side":          "buy",
+			"time_in_force": "gtc",
+		}).
+		SetHeader("Accept", "application/json").
+		SetHeader("Authorization", fmt.Sprint("Token ", os.Getenv("ROBINHOOD_TOKEN"))).
+		Post("https://api.robinhood.com/orders/")
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != 201 {
+		return fmt.Errorf("Incorrect status code: %v", resp.Status())
 	}
 
 	return nil
 }
 
 func sellOff(symbol string) error {
-	fmt.Println("SELL", symbol)
+	accountNumber, _, err := accountMetaInformation()
+	if err != nil {
+		return err
+	}
+
+	lastTradePrice, err := lastTradePriceForSymbol(symbol)
+	if err != nil {
+		return err
+	}
+
+	instrumentID, err := instrumentIDFromSymbol(symbol)
+	if err != nil {
+		return err
+	}
+
+	ownedQuantity, instrumentUrl, err := positionMetaInformation(accountNumber, instrumentID)
+	if err != nil {
+		return err
+	}
+
+	if ownedQuantity.Cmp(decimal.NewFromFloat(0.0)) != 1 {
+		return fmt.Errorf("Skipping order, don't own equity")
+	}
+
+	count := ownedQuantity.Round(0)
+	fmt.Println("SELL", count, "x", symbol, "@", lastTradePrice.Round(2))
+
+	resp, err := resty.R().
+		SetBody(map[string]interface{}{
+			"account":       fmt.Sprint("https://api.robinhood.com/accounts/", accountNumber, "/"),
+			"instrument":    instrumentUrl,
+			"symbol":        symbol,
+			"type":          "market",
+			"trigger":       "immediate",
+			"quantity":      ownedQuantity,
+			"side":          "sell",
+			"time_in_force": "gtc",
+		}).
+		SetHeader("Accept", "application/json").
+		SetHeader("Authorization", fmt.Sprint("Token ", os.Getenv("ROBINHOOD_TOKEN"))).
+		Post("https://api.robinhood.com/orders/")
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() != 201 {
+		return fmt.Errorf("Incorrect status code: %v", resp.Status())
+	}
+
 	return nil
 }
 
 func main() {
+	resty.SetRedirectPolicy(resty.FlexibleRedirectPolicy(10))
+
 	broker := os.Getenv("KAFKA_ENDPOINT")
 	topic := os.Getenv("KAFKA_TOPIC")
 	partition, err := strconv.Atoi(os.Getenv("KAFKA_PARITION"))
